d0edf865aa5838c4fe5b1d3ce4422c148e50e5bb
Yichuan Shen <shen.yichuan@gmail.com>
2019-01-21 19:24:51 +0100

    Simplify Google image search

--- commands.demo.js
+++ commands.demo.js
@@ -2,7 +2,7 @@ const {search} = require('./src/tools')
 
 module.exports = {
     g: search('https://google.com/search?q=%s', 'Perform a Google search.'),
-    gim: search('https://google.com/search?q=%s&um=1&ie=UTF-8&hl=en&tbm=isch', 'Perform a Google image search.'),
+    gim: search('https://google.com/search?q=%s&tbm=isch', 'Perform a Google image search.'),
     gm: search('https://google.com/maps/search/%s', 'Perform a Google maps search.'),
     ddg: search('https://duckduckgo.com/?q=%s', 'Perform a DuckDuckGo search.'),
     leo: search('https://dict.leo.org/german-english/%s', "Look up a word in Leo's German/English dictionary."),
@@ -12,7 +12,7 @@ module.exports = {
     ig: search('https://instagram.com/%s', 'Show profile on Instagram.'),
     yt: search('https://www.youtube.com/results?search_query=%s', 'Search on YouTube.'),
     imdb: search('https://www.imdb.com/find?q=%s', 'Search on IMDB.'),
-    ama: search('https://amazon.de/s/?field-keywords=%s', 'Search on Amazon Germany.'),
+    ama: search('https://amazon.com/s/?field-keywords=%s', 'Search on Amazon.'),
     gh: search('https://github.com/search?q=%s', 'Search on GitHub.'),
     npm: search('https://npmjs.com/package/%s', 'Go to npm package.'),
     wp: search('https://en.wikipedia.org/?search=%s', 'Search on English Wikipedia.'),
